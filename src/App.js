5afcc31e7049ff40d313e48dec9c612f6f11c3bb
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-11 19:32:27 -0700

    split menu out as a component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import React, { useState } from 'react';
-import MenuSections from './components/MenuSections';
-import ItemsList from './components/ItemsList';
-function App() {
-  const [menuSection, setMenuSection] = useState('skewers');
-  function updateMenuSection(id) {
-    setMenuSection(id);
-  }
+import Menu from './components/Menu';
+export default function App() {
   return (
-    <div className='App'>
-      <MenuSections
-        currentSection={menuSection}
-        updateMenuSection={updateMenuSection}
-      />
-      <ItemsList currentSection={menuSection} />
-    </div>
+    <>
+      <Menu />
+    </>
   );
 }
-
-export default App;
