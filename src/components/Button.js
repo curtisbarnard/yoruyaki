52573335736b873a7743d15111eb4b5fb47d6afb
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-12 15:27:36 -0700

    add password confirm

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
   return (
-    <button className='font-bold m-1 px-3 py-1 rounded-full bg-yellow-300 shadow-sm'>
+    <button className='font-bold my-4 px-3 py-1 rounded-full bg-yellow-300 shadow-sm'>
       {props.title}
     </button>
   );
