aeea2de64caa42f5530aa785cd90800fc91d5783
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-19 17:17:36 -0700

    Add routing to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import Menu from './pages/Menu';
 import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 export default function App() {
   return (
     <>
-      <Menu />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/menu' element={<Menu />} />
+        </Routes>
+      </Router>
     </>
   );
 }
