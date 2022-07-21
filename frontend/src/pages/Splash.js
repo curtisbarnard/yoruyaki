176274ac1bf8a70fe7279c9a6cacb97a12b777c1
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-20 22:12:51 -0700

    Show signin modal working

--- frontend/src/pages/Splash.js
+++ frontend/src/pages/Splash.js
@@ -2,20 +2,36 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Login from '../components/Login';
 import Button from '../components/SubmitButton';
+import ClickButton from '../components/ClickButton';
 
 export default function Splash() {
-  const [showLogin, setShowLogin] = useState('login');
+  const [showLogin, setShowLogin] = useState('hidden');
+
+  function handleClick() {
+    setShowLogin('login');
+  }
+
+  function closeWindow() {
+    setShowLogin('hidden');
+  }
 
   return (
     <>
       <div className='splash-background'>
         <div className='relative flex flex-col h-[100vh] bg-indigo-900/25 backdrop-blur-sm backdrop-brightness-50'>
-          <div className='shrink-0 flex flex-col items-center justify-center h-[80vh] text-yellow-400'>
+          <div
+            onClick={closeWindow}
+            className='shrink-0 flex flex-col items-center justify-center h-[80vh] text-yellow-400'
+          >
             <h1 className='text-5xl font-semibold pb-2'>Yoruyaki</h1>
             <span className='text-4xl font-light ml-20'>夜焼き</span>
           </div>
           <div className='shrink-0 grow flex justify-around'>
-            <Button title='Sign In' className='h-max' />
+            <ClickButton
+              title='Sign In'
+              className='h-max'
+              handleClick={handleClick}
+            />
             <Link to='/menu'>
               <Button title='See Menu' />
             </Link>
