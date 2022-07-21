4fae662160a6e53331926b66aaccec41da509508
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-21 13:16:49 -0700

    cleanup unused imports

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import ToggleButtons from '../components/ToggleButtons';
 import ItemsList from '../components/ItemsList';
-import Button from '../components/SubmitButton';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../features/auth/authSlice';
 
