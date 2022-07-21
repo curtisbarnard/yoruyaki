289870c9e2fc97f1dd3eaf70169b5a9565e0a2e2
Curtis Barnard Jr <100104319+curtisbarnard@users.noreply.github.com>
2022-07-21 14:40:35 -0700

    Fix register (#10)
    * cleanup unused imports
    
    * fix user to customer in authSlice
    
    * add comments for clarity
    
    * changed route for logging out to splash
    
    * remove postbuild for heroku from package.json
    
    * add heroku postbuild back - testing with mongo whitelist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import Menu from './pages/Menu';
-import Login from './components/Login';
-import Register from './components/Register';
 import Splash from './pages/Splash';
 import Admin from './pages/Admin';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -10,8 +8,6 @@ export default function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
           <Route path='/' element={<Splash />} />
           <Route path='/menu' element={<Menu />} />
           <Route path='/admin' element={<Admin />} />
