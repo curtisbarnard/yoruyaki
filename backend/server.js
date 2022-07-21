a114bf7810f4cb9b90950c5c723873a7eb1d469a
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-20 17:25:45 -0700

    deployment tweaks

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -12,6 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/inventory', require('./routes/inventoryRoutes'));
 app.use('/api/customers', require('./routes/customerRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set node to production'));
+}
 
 app.use(errorHandler);
 
