df0538e834879c1acde9bf23d3b1a1e38f4353e2
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-07-22 17:49:57 -0700

    update styling of inventory

--- frontend/src/components/InventorySection.js
+++ frontend/src/components/InventorySection.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getInventory } from '../features/inventory/inventorySlice';
+import Loading from './Loading';
 
 export default function InventorySection() {
   // Redux variables
@@ -23,32 +24,52 @@ export default function InventorySection() {
   const inputStyling =
     'px-2 py-2 text-lg border-b-2 border-indigo-100 focus:outline-8 focus:outline-indigo-400';
 
-  // TODO sort inventory items alphabetically and by class
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  // Sort inventory items alphabetically and by category
+  const sortedInventory = [...inventory].sort((a, b) => {
+    if (a.category === b.category) {
+      alphaSort(a, b);
+    }
+    return a.category.charCodeAt(0) - b.category.charCodeAt(0);
+  });
+
+  function alphaSort(a, b) {
+    return a.itemName.charCodeAt(0) - b.itemName.charCodeAt(0);
+  }
+
   // Map inventory items to components list
-  const inventoryItems = inventory.map((item) => {
+  const inventoryItems = sortedInventory.map((item) => {
     return (
-      <form className='w-full flex justify-around'>
-        <input
-          type='number'
-          value={item.stock}
-          className={`${inputStyling} w-[75px]`}
-        />
-        <input
-          type='text'
-          value={item.itemName}
-          className={`${inputStyling} font-semibold`}
-        />
-        <input
-          type='text'
-          value={item.itemDesc}
-          className={`${inputStyling} w-1/3`}
-        />
-        <input
-          type='text'
-          value={item.category}
-          className={`${inputStyling} `}
-        />
-      </form>
+      <div key={item._id} className='w-full grid grid-cols-12 px-10'>
+        <button key={`edit ${item._id}`}>Edit</button>
+        <div
+          className={`${inputStyling} text-center`}
+          key={item._id + item.stock}
+        >
+          {item.stock}
+        </div>
+        <div
+          className={`${inputStyling} font-semibold col-span-2`}
+          key={item._id + item.itemName}
+        >
+          {item.itemName}
+        </div>
+        <div
+          className={`${inputStyling} col-span-6`}
+          key={item._id + item.itemDesc}
+        >
+          {item.itemDesc}
+        </div>
+        <div
+          className={`${inputStyling} col-span-2`}
+          key={item._id + item.category}
+        >
+          {item.category}
+        </div>
+      </div>
     );
   });
   // JSX to Render
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,4 +1,3 @@
-import InventoryForm from '../components/InventoryForm';
 import ToggleButtons from '../components/ToggleButtons';
 import InventorySection from '../components/InventorySection';
 
@@ -9,12 +8,16 @@ export default function Admin() {
     <>
       <h1>Yoruyaki</h1>
       <h2>Admin Panel</h2>
-      <ToggleButtons sections={sections} />
-      <div className='flex justify-around py-2 text-2xl font-semibold'>
-        <span>QTY</span>
-        <span>Item Name</span>
-        <span className='w-1/4'>Description</span>
-        <span>Category</span>
+      <div className='mx-auto max-w-md'>
+        <ToggleButtons sections={sections} />
+      </div>
+
+      <div className='grid grid-cols-12 px-10 py-2 text-2xl font-semibold'>
+        <div></div>
+        <span className='text-center'>QTY</span>
+        <span className='col-span-2'>Item Name</span>
+        <span className='col-span-6'>Description</span>
+        <span className='col-span-2'>Category</span>
       </div>
       <InventorySection />
     </>
