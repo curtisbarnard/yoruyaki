47f32fe39cf572c5f04f61158cabe5f07ab36707
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-08-07 09:47:51 -0700

    remove margin from button

--- frontend/src/components/ClickButton.js
+++ frontend/src/components/ClickButton.js
@@ -1,7 +1,7 @@
 export default function ClickButton(props) {
   return (
     <button
-      className={`${props.className} text-xl font-bold my-4 px-4 py-2 rounded-full bg-yellow-300 shadow-sm`}
+      className={`${props.className} text-xl font-bold px-4 py-2 rounded-full bg-yellow-300 shadow-sm`}
       onClick={props.handleClick}
     >
       {props.title}
