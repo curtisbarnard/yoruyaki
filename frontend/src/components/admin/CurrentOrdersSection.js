95a664a997331da44bbed2677214f9337f0be7b9
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-13 13:41:46 -0700

    breakout order card as a component

--- frontend/src/components/admin/CurrentOrdersSection.js
+++ frontend/src/components/admin/CurrentOrdersSection.js
@@ -1,7 +1,8 @@
+import { produceWithPatches } from 'immer';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllOrders, markOrderComplete, reset } from '../../features/order/orderSlice';
-import ClickButton from '../ClickButton';
+import OrderCard from './OrderCard';
 
 export default function CurrentOrdersSection() {
   // setup redux variables
@@ -30,29 +31,16 @@ export default function CurrentOrdersSection() {
 
   // Creating the array of order cards
   const ordersArray = openOrders.map((order) => {
-    const orderItems = order.orderItems.map((item) => {
-      return (
-        <li className='flex justify-between text-lg py-1 px-4' key={order._id + item.itemName}>
-          <span>{item.itemName}</span>
-          <span>{item.qty}</span>
-        </li>
-      );
-    });
+    console.log(order);
     return (
-      <div
-        className='grid grid-cols-2 content-start items-baseline bg-indigo-900
-         rounded-lg shadow-md p-4'
-        key={order._id}
+      <OrderCard
         id={order._id}
-      >
-        <h3 className='text-2xl text-yellow-300 font-semibold'>#{order._id.slice(-8)}</h3>
-        <span className='text-yellow-300'>{order.customerName}</span>
-        <ul className='col-span-2 p-2 bg-indigo-50 rounded-md mt-2'>{orderItems}</ul>
-        <ClickButton className='mt-4' title='Done' handleClick={markComplete} />
-        <span className='text-yellow-300 text-xl self-end justify-self-end'>
-          ${order.totalPrice}
-        </span>
-      </div>
+        customerName={order.customerName}
+        totalPrice={order.totalPrice}
+        orderItems={order.orderItems}
+        orderStatus={order.orderStatus}
+        handleClick={markComplete}
+      />
     );
   });
 
---
+++ frontend/src/components/admin/OrderCard.js
@@ -0,0 +1,29 @@
+import ClickButton from '../ClickButton';
+
+export default function OrderCard(props) {
+  const orderItems = props.orderItems.map((item) => {
+    return (
+      <li className='flex justify-between text-lg py-1 px-4' key={props.id + item.itemName}>
+        <span>{item.itemName}</span>
+        <span>{item.qty}</span>
+      </li>
+    );
+  });
+  return (
+    <div
+      className='grid grid-cols-2 content-start items-baseline bg-indigo-900
+       rounded-lg shadow-md p-4'
+      key={props.id}
+      id={props.id}
+    >
+      <h3 className='text-2xl text-yellow-300 font-semibold'>#{props.id.slice(-8)}</h3>
+      <span className='text-yellow-300'>{props.customerName}</span>
+      <ul className='col-span-2 p-2 bg-indigo-50 rounded-md mt-2'>{orderItems}</ul>
+      {/* Conditional below allows card to be used on the past orders page */}
+      {props.orderStatus !== 'completed' && (
+        <ClickButton className='mt-4' title='Done' handleClick={props.handleClick} />
+      )}
+      <span className='text-yellow-300 text-xl self-end justify-self-end'>${props.totalPrice}</span>
+    </div>
+  );
+}
