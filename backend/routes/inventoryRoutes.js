37a5d052143f35cdaec0be4d124856f097f41115
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-09-14 16:06:33 -0700

    add route and controller for depleting inventory stock

--- backend/routes/inventoryRoutes.js
+++ backend/routes/inventoryRoutes.js
@@ -4,11 +4,13 @@ const {
   getInventory,
   createInventory,
   updateInventory,
+  depleteInventory,
   deleteInventory,
 } = require('../controllers/inventoryController');
 const { protect } = require('../middleware/authmiddleware');
 
 router.route('/').get(getInventory).post(createInventory);
 router.route('/:id').put(updateInventory).delete(deleteInventory);
+router.route('/deplete/:id').put(depleteInventory);
 
 module.exports = router;
